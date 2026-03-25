6457c8c07ca9fead20f05ea21ffbe5b74860e053
Rémi BUISSON <remi.buisson@traefik.io>
2026-03-25 17:22:51 +0100

    chore(deps): replace cobra with urfave/cli v3 (#17)

--- cmd/root.go
+++ cmd/root.go
@@ -9,88 +9,95 @@ import (
 	"strings"
 
 	"github.com/rs/zerolog"
-	"github.com/spf13/cobra"
 	"github.com/traefik/helm-changelog/pkg/git"
 	"github.com/traefik/helm-changelog/pkg/helm"
 	"github.com/traefik/helm-changelog/pkg/output"
+	"github.com/urfave/cli/v3"
 )
 
-var changelogFilename string
-
-// rootCmd represents the base command when called without any subcommands.
-var rootCmd = &cobra.Command{
-	Use:   "helm-changelog",
-	Short: "Create changelogs for Helm Charts, based on git history",
-	Run: func(_ *cobra.Command, _ []string) {
-		ctx := context.Background()
-		log := zerolog.Ctx(ctx)
-
-		currentDir, err := os.Getwd()
-		if err != nil {
-			log.Fatal().Err(err).Msg("failed to get working directory")
-		}
-
-		g := &git.Git{Log: log}
-
-		gitBaseDir, err := g.FindGitRepositoryRoot(ctx)
-		if err != nil {
-			log.Fatal().Msg(
-				"Could not determine git root directory. helm-changelog depends largely on git history.",
-			)
-		}
-
-		fileList, err := helm.FindCharts(currentDir)
-		if err != nil {
-			log.Fatal().Err(err).Msg("failed to find charts")
-		}
-
-		for _, chartFileFullPath := range fileList {
-			log.Info().Msgf("Handling: %s", chartFileFullPath)
-
-			fullChartDir := filepath.Dir(chartFileFullPath)
-			chartFile := strings.TrimPrefix(chartFileFullPath, gitBaseDir+"/")
-			relativeChartFile := strings.TrimPrefix(chartFileFullPath, currentDir+"/")
-			relativeChartDir := filepath.Dir(relativeChartFile)
-
-			allCommits, err := g.GetAllCommits(ctx, fullChartDir)
+// Execute runs the helm-changelog CLI.
+func Execute() {
+	app := &cli.Command{
+		Name:  "helm-changelog",
+		Usage: "Create changelogs for Helm Charts, based on git history",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "filename",
+				Aliases: []string{"f"},
+				Value:   "Changelog.md",
+				Usage:   "Filename for changelog",
+			},
+			&cli.StringFlag{
+				Name:    "verbosity",
+				Aliases: []string{"v"},
+				Value:   zerolog.WarnLevel.String(),
+				Usage:   "Log level (trace, debug, info, warn, error, fatal, panic)",
+			},
+		},
+		Before: func(ctx context.Context, cmd *cli.Command) (context.Context, error) {
+			log, err := setUpLogs(cmd.String("verbosity"))
 			if err != nil {
-				log.Fatal().Err(err).Msg("failed to get commits")
+				return ctx, err
 			}
 
-			releases := helm.CreateHelmReleases(ctx, log, chartFile, relativeChartDir, g, allCommits)
-
-			changeLogFilePath := filepath.Join(fullChartDir, changelogFilename)
-			output.Markdown(log, changeLogFilePath, releases)
-		}
-	},
-}
-
-// Execute sets all flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
-	var v string
-
-	rootCmd.PersistentPreRunE = func(cmd *cobra.Command, _ []string) error {
-		log, err := setUpLogs(v)
-		if err != nil {
-			return err
-		}
-
-		ctx := log.WithContext(cmd.Context())
-		cmd.SetContext(ctx)
-
-		return nil
+			return log.WithContext(ctx), nil
+		},
+		Action: run,
 	}
 
-	rootCmd.PersistentFlags().StringVarP(
-		&changelogFilename, "filename", "f", "Changelog.md", "Filename for changelog",
-	)
-	rootCmd.PersistentFlags().StringVarP(
-		&v, "verbosity", "v", zerolog.WarnLevel.String(),
-		"Log level (trace, debug, info, warn, error, fatal, panic)",
-	)
+	err := app.Run(context.Background(), os.Args)
+	if err != nil {
+		log := zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr}).
+			With().
+			Timestamp().
+			Logger()
+		log.Fatal().Err(err).Msg("")
+	}
+}
 
-	cobra.CheckErr(rootCmd.Execute())
+func run(ctx context.Context, cmd *cli.Command) error {
+	log := zerolog.Ctx(ctx)
+	changelogFilename := cmd.String("filename")
+
+	currentDir, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("getting working directory: %w", err)
+	}
+
+	g := &git.Git{Log: log}
+
+	gitBaseDir, err := g.FindGitRepositoryRoot(ctx)
+	if err != nil {
+		return fmt.Errorf(
+			"could not determine git root directory; helm-changelog depends largely on git history: %w", err,
+		)
+	}
+
+	fileList, err := helm.FindCharts(currentDir)
+	if err != nil {
+		return fmt.Errorf("finding charts: %w", err)
+	}
+
+	for _, chartFileFullPath := range fileList {
+		log.Info().Msgf("Handling: %s", chartFileFullPath)
+
+		fullChartDir := filepath.Dir(chartFileFullPath)
+		chartFile := strings.TrimPrefix(chartFileFullPath, gitBaseDir+"/")
+		relativeChartFile := strings.TrimPrefix(chartFileFullPath, currentDir+"/")
+		relativeChartDir := filepath.Dir(relativeChartFile)
+
+		allCommits, err := g.GetAllCommits(ctx, fullChartDir)
+		if err != nil {
+			return fmt.Errorf("getting commits: %w", err)
+		}
+
+		releases := helm.CreateHelmReleases(ctx, log, chartFile, relativeChartDir, g, allCommits)
+
+		changeLogFilePath := filepath.Join(fullChartDir, changelogFilename)
+		output.Markdown(log, changeLogFilePath, releases)
+	}
+
+	return nil
 }
 
 // setUpLogs configures a zerolog logger and returns it.
--- go.mod
+++ go.mod
@@ -4,19 +4,17 @@ go 1.25.8
 
 require (
 	github.com/rs/zerolog v1.34.0
-	github.com/spf13/cobra v1.10.2
 	github.com/stretchr/testify v1.11.1
+	github.com/urfave/cli/v3 v3.8.0
 	go.yaml.in/yaml/v4 v4.0.0-rc.4
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/spf13/pflag v1.0.10 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
 	golang.org/x/sys v0.42.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
